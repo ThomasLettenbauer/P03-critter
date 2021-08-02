3f61561ed6b55e68e36921eca68c1a53935b3502
thomas <thomas.lettenbauer@gmail.com>
2021-08-02 17:14:16 +0200

    Functional Tests o.k., Postman Collection o.k.

--- src/main/java/com/udacity/jdnd/course3/critter/pet/Pet.java
+++ src/main/java/com/udacity/jdnd/course3/critter/pet/Pet.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 public class Pet {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private PetType type;
--- src/main/java/com/udacity/jdnd/course3/critter/user/EmployeeRequest.java
+++ src/main/java/com/udacity/jdnd/course3/critter/user/EmployeeRequest.java
@@ -8,7 +8,7 @@ import java.util.Set;
 public class EmployeeRequest {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
     @ElementCollection(targetClass=EmployeeSkill.class)
